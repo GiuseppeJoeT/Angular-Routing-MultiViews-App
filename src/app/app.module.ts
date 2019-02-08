92bece0ce18abd1130995aa1c0e46fcfd549c83f
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-08 17:32:35 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
 import { ContactsComponent } from './features/contacts/contacts.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { UsersComponent } from './features/users/users.component';
 import { UserDetailsComponent } from './features/user-details/user-details.component';
+
+import { Utility } from './features/users/services/utility';
+import { UserService } from './services/user.service';
 
 
 @NgModule({
@@ -23,7 +28,8 @@ import { UserDetailsComponent } from './features/user-details/user-details.compo
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2CarouselamosModule
     // RouterModule.forRoot([
     //   { path: 'catalog', component: CatalogComponent },
     //   { path: 'contacts', component: ContactsComponent },
@@ -31,7 +37,10 @@ import { UserDetailsComponent } from './features/user-details/user-details.compo
       // { path: '**', redirectTo: 'home'}
     // ])
   ],
-  providers: [],
+  providers: [
+    Utility,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
