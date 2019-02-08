dc653e379f12ff7b7916d3f7f8a04311cf5c337e
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-08 17:32:45 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,45 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <app-navbar></app-navbar>
-  <router-outlet></router-outlet>
-  `
-  ,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'MultiView-AngularRouter';
+
+  // Carousel
+  items: Array<any> = [];
+
+  constructor() {
+    // Carousel
+    this.items = [
+      { name: 'assets/images/con-te-logo.svg' },
+      { name: 'assets/images/Oracle_logo.png' },
+      { name: 'assets/images/skaffolder_logo.jpg' },
+      { name: 'assets/images/con-te-logo.svg' },
+      { name: 'assets/images/Oracle_logo.png' },
+      { name: 'assets/images/skaffolder_logo.jpg' }
+    ];
+  }
+
+  users = [];
+
+  add(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    // inviare all'array nuovo valore
+    this.users.push({
+      name: inputName.value,
+      age: inputAge.value
+    });
+
+    // pulire campo input dopo invio
+    inputName.value = '';
+    inputAge.value = '';
+
+    // focus su input dopo click bottone
+    inputName.focus();
+  }
+
+
+
 }
