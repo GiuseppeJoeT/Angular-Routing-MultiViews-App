9994bad09e3ed674db80faf7188830e9c6b18a54
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-08 20:16:15 +0100

    catalog

---
+++ src/app/features/catalog/catalog.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+
+  constructor() {
+  //   users = [];
+
+  //   add(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+  //     // inviare all'array nuovo valore
+  //     this.users.push({
+  //       name: inputName.value,
+  //       age: inputAge.value
+  //     });
+
+  //     // pulire campo input dopo invio
+  //     inputName.value = '';
+  //     inputAge.value = '';
+
+  //     // focus su input dopo click bottone
+  //     inputName.focus();
+  //   }
+  }
+
+
+}
