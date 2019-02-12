754f32316c35c26064f11ddcf310775fc7fee8c6
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-12 12:54:19 +0100

    Update users.component.ts

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent {
   // we shoud create a users interface then
-  users: any[];
+  users: User[];
 
   constructor(http: HttpClient) {
-    http.get<any[]>('https://jsonplaceholder.typicode.com/users')
-      .subscribe(res => this.users = res);
-  }
+    setTimeout(() => {
+      // attraverso HttpClient possiamo accedere ai metodi: GET, POST, PUT, PATCH, DELETE
+      http.get<any[]>('https://jsonplaceholder.typicode.com/users')
+        .subscribe(result => this.users = result);
 
-  ngOnInit() {
+    }, 3000);
   }
 
 }
