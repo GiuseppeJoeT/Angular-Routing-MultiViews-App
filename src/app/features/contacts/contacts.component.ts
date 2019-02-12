0eb67b41ce3c1d17fcfc727f9159fbc2b3133b6f
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-12 12:55:29 +0100

    Update contacts.component.ts

--- src/app/features/contacts/contacts.component.ts
+++ src/app/features/contacts/contacts.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  contactUsers = [
+    { id: 1, name: 'Fabio', gender: 'M' },
+    { id: 2, name: 'Lisa', gender: 'F' },
+    { id: 3, name: 'Lorenzo', gender: 'M' },
+    { id: 4, name: 'Silvia', gender: 'F' }
+  ];
 }
