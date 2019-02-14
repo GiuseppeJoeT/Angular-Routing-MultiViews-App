f3aa9155a14cc6b9c6b145fc26c5d0219e06d042
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-14 18:43:42 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,30 +12,33 @@ export class AppComponent {
   // Carousel
   items: Array<any> = [];
 
-  constructor() {
+  constructor( ) {
     // Carousel
     this.items = [
       { name: 'assets/images/con-te-logo.svg' },
       { name: 'assets/images/Oracle_logo.png' },
-      { name: 'assets/images/skaff_logo.jpg' },
+      { name: 'assets/images/skaffolder_logo.jpg' },
       { name: 'assets/images/con-te-logo.svg' },
       { name: 'assets/images/Oracle_logo.png' },
-      { name: 'assets/images/skaff_logo.jpg' }
+      { name: 'assets/images/skaffolder_logo.jpg' }
     ];
   }
 
   users = [];
 
-  add(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+  // Input, Template reference variable,
+  add(inputName: HTMLInputElement, inputAge: HTMLInputElement, inputCity: HTMLInputElement) {
     // inviare all'array nuovo valore
     this.users.push({
       name: inputName.value,
-      age: inputAge.value
+      age: inputAge.value,
+      city: inputCity.value
     });
 
     // pulire campo input dopo invio
     inputName.value = '';
     inputAge.value = '';
+    inputCity.value = '';
 
     // focus su input dopo click bottone
     inputName.focus();
