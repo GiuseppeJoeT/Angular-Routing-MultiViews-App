b478e102c25c5fda0b2e89b19231f41e57a40b20
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-14 18:44:00 +0100

    Update catalog.component.ts

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../model/user.interface'
 
 @Component({
   selector: 'app-catalog',
@@ -6,13 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
-
+  // 1way vs 2 way binding
   label = 'Username';
 
   constructor() {
     setTimeout(() => {
       this.label = '(minimo 3 caratteri)';
     }, 2000);
+  }
+
+  // ngForm, ngModel e gestione data model
+  utente: User;
+
+  utenti = [
+    { name: 'Joe', age: 32, city: 'Rome' }
+  ];
+
+  add(form) {
+    // console.log(form.value);
+    this.utenti.push(form.value);
+  }
+
+  setActive(utente: User) {
+    this.utente = utente;
   }
 
 }
