4ad2b2d066d3183f9f9669f382bc04d1283a8a7e
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-19 17:07:56 +0100

    Update contacts.component.ts

--- src/app/features/contacts/contacts.component.ts
+++ src/app/features/contacts/contacts.component.ts
@@ -8,9 +8,26 @@ import { Component } from '@angular/core';
 export class ContactsComponent  {
 
   contactUsers = [
-    { id: 1, name: 'Fabio', gender: 'M' },
-    { id: 2, name: 'Lisa', gender: 'F' },
-    { id: 3, name: 'Lorenzo', gender: 'M' },
-    { id: 4, name: 'Silvia', gender: 'F' }
+    { id: 1, name: 'Fabio', gender: 'M', age: 20},
+    { id: 2, name: 'Lisa', gender: 'F', age: 30 },
+    { id: 3, name: 'Lorenzo', gender: 'M', age: 40 },
+    { id: 4, name: 'Silvia', gender: 'F', age: 50 }
   ];
+
+  // usare direttiva [ngClass]
+  getFemaleBiggerFont(user) {
+    return {
+      'female': user.gender === 'F',
+      'BiggerFont': true
+  };
 }
+
+getStyle(user) {
+  return {
+    color: user.gender === 'M' ? 'blue' : 'pink',
+    backgroundColor: '#b3b3ff',
+    fontSize: user.age + 'px'
+  };
+}
+
+} // close ContactsComponent
