ae0d4516a33a8c7debf6fb4c9e5fbac9e93502fa
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-07 11:54:28 +0200

    Router - Compontent call by API
    http.GET

--- src/app/features/user-details/user-details.component.ts
+++ src/app/features/user-details/user-details.component.ts
@@ -1,34 +1,45 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
   user;
+  id: number;
 
   constructor(
     activatedRoute: ActivatedRoute,
-    http: HttpClient
+    http: HttpClient,
+    // Invece di invocare nuova pagina con routerLink="/users", lo faremo tramite API
+    // inovando il servizio/classe Router
+    // Send the given `HttpRequest` and return a stream of `HttpEvents`
+    private router: Router
   ) {
-     // +, casting: string to number
-     const id = +activatedRoute.snapshot.params['id'];
+    //  const id = +activatedRoute.snapshot.params['id']; -> casting
     // console.log(activatedRoute.snapshot['id']);
-
-    http.get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .subscribe(res => {
-        this.user = res;
-        // metttere dentro function
-
-        // servizio chiamata http
-        // servizio controllo dati: get().users, set().users, clear().users
+    // la proprietà .params dell'interfaccia  activatedRoute resistuisce un Observable
+    activatedRoute.params
+      .subscribe(params => {
+        // log id user attuale
+        console.log(params['id']);
+        // +, casting: string to number
+        this.id = +params['id'];
+          http.get(`https://jsonplaceholder.typicode.com/users/${this.id}`)
+            .subscribe(resp => {
+              this.user = resp;
       });
+    });
   }
 
-  ngOnInit() {
+  goToNextUser() {
+    // console.log(user);
+    // nextId è il prossimo "id" (tipo number)
+    const nextId = this.id + 1;
+    this.router.navigateByUrl(`users/${nextId}`);
   }
 
 }
