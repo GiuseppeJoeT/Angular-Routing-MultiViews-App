662cf2c7fb3f63a219b1eff2b39f7824360abb35
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-07 11:54:57 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
@@ -14,7 +15,8 @@ import { UsersComponent } from './features/users/users.component';
 import { UserDetailsComponent } from './features/user-details/user-details.component';
 import { UserService } from './core/services/user.service';
 import { FormValidationComponent } from './features/form-validation/form-validation.component';
-
+import { CardComponent } from './shared/card/card.component';
+import { CollapsPanelComponent } from './shared/collaps-panel/collaps-panel.component';
 
 @NgModule({
   declarations: [
@@ -24,14 +26,18 @@ import { FormValidationComponent } from './features/form-validation/form-validat
     NavbarComponent,
     UsersComponent,
     UserDetailsComponent,
-    FormValidationComponent
+    FormValidationComponent,
+    CardComponent,
+    CollapsPanelComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
     Ng2CarouselamosModule
+    // app routing alternative
     // RouterModule.forRoot([
     //   { path: 'catalog', component: CatalogComponent },
     //   { path: 'contacts', component: ContactsComponent },
